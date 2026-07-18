30dadd3b6f6778978a7d01b2925ffb765e184414
zer0sik2 <yoon0sik2@gmail.com>
2026-07-18 17:53:38 +0900

    Apply ChargeBell 60-30-10 dark palette

--- app/src/main/java/io/github/zer0sik2/chargebell/ui/theme/Color.kt
+++ app/src/main/java/io/github/zer0sik2/chargebell/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package io.github.zer0sik2.chargebell.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/**
+ * ChargeBell의 60 / 30 / 10 컬러 규칙이다.
+ *
+ * 60%: 화면 배경처럼 가장 넓은 영역에 사용하는 검정 계열
+ * 30%: 카드와 보조 표면에 사용하는 짙은 회색, 주요 조작부에 사용하는 주황 계열
+ * 10%: 텍스트와 아이콘에 사용하는 흰색 계열
+ */
+val ChargeBlack = Color(0xFF0D0D0D)
+val ChargeDarkGray = Color(0xFF1A1A1A)
+val ChargeSurfaceGray = Color(0xFF242424)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ChargeOrange = Color(0xFFFF7A00)
+val ChargeOrangeContainer = Color(0xFF8A3F00)
+
+val ChargeWhite = Color(0xFFFFFFFF)
+val ChargeSoftWhite = Color(0xFFE8E8E8)
+val ChargeDisabledGray = Color(0xFF8A8A8A)
+val ChargeOutlineGray = Color(0xFF5A5A5A)
+val ChargeError = Color(0xFFFF6B6B)
